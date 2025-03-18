ff3a6bd9d425ce93ece8717d57d2d474fd86ac74
Alhilal Anwar <alhilalanwar07@gmail.com>
2025-03-18 23:59:17 +0800

    update db model

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -58,8 +58,8 @@
             <footer class="footer">
                 <div class="container-fluid d-flex justify-content-between">
                     <div class="copyright">
-                        2024, made with <i class="fa fa-heart heart text-danger"></i> by
-                        <a href="#">KOMINFO</a>
+                        {{ date('Y') }}, made with <i class="fa fa-heart heart text-danger"></i> by
+                        <a href="#">YOU</a>
                     </div>
                 </div>
             </footer>
