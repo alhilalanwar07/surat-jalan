e64277daec48af021da035fe2086e771ad891aa9
Alhilal Anwar <alhilalanwar07@gmail.com>
2025-03-19 00:02:29 +0800

    update db model

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,7 @@ Auth::routes(['register' => false, 'reset' => false]);
 // jika ke /, redirect ke /login
 Route::redirect('/', '/login');
 
-Auth::routes();
-
 Route::middleware('auth')->group(function () {
-    Route::view('kendaraan', 'kendaraan')->name('kendaraan');
     Route::view('dashboard', 'dashboard')->name('home');
     Route::view('manajemen-user', 'manajemen-user')->name('admin.manajemen-user');
     Route::view('profil', 'profil')->name('profil');
